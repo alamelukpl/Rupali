5fcd355171aa1d36ce7c9e9dbf3d53b72f1aa652
Alamelu Kapali <alamelukpl@gmail.com>
2022-06-13 20:16:05 +0200

    commited

---
+++ Animal.java
@@ -0,0 +1,32 @@
+
+public class Animal {
+	
+	 String name = "Animals";
+	int sid = 1;
+	
+	public void eyes()
+	{
+		System.out.println("Seeeing");
+	}
+	
+	public void add(int x,int y)
+	{
+		System.out.println(x+y);
+	}
+	
+	public void add1(int x,int y)
+	{
+		System.out.println(x+y);
+	}
+	
+	public void ears()
+	{
+		System.out.println("Hearing");
+	}
+	
+	public void legs()
+	{
+		System.out.println("walking");
+	}
+
+}
